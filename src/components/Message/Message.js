f03ec3921d896b63905692046989cb6dfd0027ff
tearofenemy <calm.snow98@gmail.com>
2021-03-05 13:28:57 +0300

    add date-fns lib, for render wher msg was sent, also setup personal message from user, add read icon

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import classNames from 'classnames'
+import {formatDistanceToNow} from 'date-fns'
+import ruLocale from 'date-fns/locale/ru'
 
 import './Message.scss'
+import readed from '../../assets/readed.svg'
+import noreaded from '../../assets/noreaded.svg'
 
-const Message = ({avatar, user, text, date}) => (
-    <div className="message">
-        <div className="message__avatar">
-            <img src={avatar} alt={`Avatar ${user}`}/>
-        </div>
+const Message = ({avatar, user, text, date, isMe, isReaded}) => (
+    <div className={classNames('message', {'message--isMe': isMe})}> 
         <div className="message__content">
-            <div className="message__bubble">
-                <div className="message__text">
-                    <p>{text}</p>
-                </div>
+            {isMe && isReaded ?
+             (<img className="message__icon-readed" src={readed} alt="Readed icon"/>)
+            :(<img className="message__icon-readed" src={noreaded} alt="Readed icon"/>)}
+            <div className="message__avatar">
+                <img src={avatar} alt={`Avatar ${user}`}/>
             </div>
-            <span className="message__date">{date}</span>
-        </div>
-    </div>
+            <div className="message__info">
+                <div className="message__bubble">
+                    <div className="message__text">
+                        <p>{text}</p>
+                    </div>
+                </div>
+                <span className="message__date">
+                    {formatDistanceToNow(date, {addSuffix: true, locale: ruLocale})}
+                </span>
+            </div>
+        </div> 
+    </div>  
 )
 
 Message.defaultProp = {
